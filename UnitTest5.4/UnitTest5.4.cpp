433165db0316f561e5ac18f0d1307559cb096085
Олександр <oleksandr140407@gmail.com>
2024-10-09 02:30:23 +1000

    Lab5.4

---
+++ UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double s = (1.0 / 1) + sqrt(1 + pow(sin(1), 2));
+			Assert::AreEqual(s, 2.30, 0.01);
+		}
+	};
+}
